9004ce7d0d965feef380b854733123b4bc7306a2
flyptkarsh <ptkarsh@gmail.com>
2016-07-15 14:34:43 -0400

    s3 config

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-== Harry Trent 
+== Harry Trent
 
-Prototype for "The Brichbox of Condoms and lube"
+Prototype for "Sample Store for Portfolio"
 
 Ruby 2.3.0p0 (2015-12-25 revision 53290)
 Rails 4.2.6
--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -28,7 +28,7 @@ Spree.config do |config|
   # Frontend:
 
   # Custom logo for the frontend
-  # config.logo = "logo/solidus_logo.png"
+  config.logo = "logo/solidus_logo.png"
 
   # Template to use when rendering layout
   # config.layout = "spree/layouts/spree_application"
@@ -37,7 +37,7 @@ Spree.config do |config|
   # Admin:
 
   # Custom logo for the admin
-  # config.admin_interface_logo = "logo/solidus_logo.png"
+  config.admin_interface_logo = "logo/solidus_logo.png"
 
   # Gateway credentials can be configured statically here and referenced from
   # the admin. They can also be fully configured from the admin.
@@ -51,6 +51,38 @@ Spree.config do |config|
   #   test_mode: !Rails.env.production?
   # )
 end
+
+attachment_config = {
+
+  s3_credentials: {
+    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    bucket:            ENV['S3_BUCKET_NAME']
+  },
+
+  storage:        :s3,
+  s3_headers:     { "Cache-Control" => "max-age=31557600" },
+  s3_protocol:    "https",
+  bucket:         ENV['S3_BUCKET_NAME'],
+  url:            ":s3_domain_url",
+
+  styles: {
+      mini:     "48x48>",
+      small:    "100x100>",
+      product:  "240x240>",
+      large:    "600x600>"
+  },
+
+  path:           "/:class/:id/:style/:basename.:extension",
+  default_url:    "/:class/:id/:style/:basename.:extension",
+  default_style:  "product"
+}
+
+attachment_config.each do |key, value|
+  Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+end
+
+
 
 Spree::Frontend::Config.configure do |config|
   config.use_static_preferences!
